48a814de96ddcf7efd619da0256838e3d104ddc8
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2019-12-26 21:17:40 +0100

    Added setting of x11 keyboard.

--- 30-desktop/32-install-xorg.sh
+++ 30-desktop/32-install-xorg.sh
@@ -5,3 +5,9 @@ sudo pacman -S --noconfirm \
 	xorg-apps \
 	xorg-xinit \
 	xorg-xrandr
+
+echo -n "Set x11 keyboard layout: [dk]"
+read KEYBOARD_LAYOUT
+echo -n "Set x11 keyboard model: [pc105]"
+read KEYBOARD_MODEL
+sudo localectl --no-convert set-x11-keymap ${KEYBOARD_LAYOUT:-dk} ${KEYBOARD_MODEL:-pc105}
