346c71ed45412588e650a4aa616e072d21af78f7
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2019-12-26 20:31:11 +0100

    Added set-up of yay.

---
+++ 10-utils/12-setup-yay.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+sudo pacman -S --noconfirm binutils make gcc pkg-config fakeroot
+
+echo -n "Specify where to clone yay: [./]"
+read CLONE_DIR
+
+if [[ -z CLONE_DIR ]]; then
+	cd $CLONE_DIR
+fi
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
