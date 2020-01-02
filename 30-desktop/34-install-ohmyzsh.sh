5fd6107671b0715dcf720fcea56abda7e9eb69e0
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2020-01-02 22:06:32 +0100

    Added zsh-autosuggestions.

--- 30-desktop/34-install-ohmyzsh.sh
+++ 30-desktop/34-install-ohmyzsh.sh
@@ -4,4 +4,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
