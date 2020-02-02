fc3df0aa33e06e78b65bef0942beff435c4a27d0
Mikael Elkiaer <2102306+MikaelElkiaer@users.noreply.github.com>
2020-02-02 21:25:53 +0100

    Added zsh-z.

--- 30-desktop/34-install-ohmyzsh.sh
+++ 30-desktop/34-install-ohmyzsh.sh
@@ -7,3 +7,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+
